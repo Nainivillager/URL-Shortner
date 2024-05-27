c8ae20354472e04d4970a1f5a52458b523551598
Nainivillager <himanshubhat358@gmail.com>
2024-05-27 16:34:22 +0530

    dynamically changing fields in django

--- URL_shortner/myapp/views.py
+++ URL_shortner/myapp/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+
+
+
 
 def hello_world(request):
    return HttpResponse("Hello World!")
 
 def home_page(request):
+
+   context = {
+      "submitted": False,
+      "current_date": timezone.now().date()
+   }
+
    if (request.method) == 'POST':
+
+      context["submitted"] = True
+
       data = request.POST
       long_url = data['longurl']
       custom_name = data['custom_name']
 
       print(long_url)
       print(custom_name)
+
+      context["long_url"] = long_url
+      context["short_url"] = request.build_absolute_uri() + custom_name
+
+      
    else:
       print("No data sent by user")
 
-   return render(request, "index.html")
+   
+
+   return render(request, "index.html", context)
 
 
 # def task_page(request):
